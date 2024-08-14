0796f45130bda6a239b1474ba6c454bc7cb2e90d
Pavel-Kuznetsov-111 <gilrok@rambler.ru>
2024-08-14 23:16:34 +0300

    Проверь пожалуйста Homework_10.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Задача №1");
+        String firstName = " Ivan ";
+        String middleName = " Ivanovich";
+        String lastName = " Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О. сотрудника " + fullName + ". ");
+        System.out.println();
+        System.out.println("Задача №2");
+        String small = fullName;
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + small.toUpperCase());
+        System.out.println();
+        System.out.println("Задача №3");
+        String firstName2 = " Семён  ";
+        String middleName2 = " Семёнович ";
+        String lastName2 = " Иванов ";
+        String fullName2 = lastName2 + firstName2 + middleName2;
+        fullName2 = fullName2.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
+
     }
 }
