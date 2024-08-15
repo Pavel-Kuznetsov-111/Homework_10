f3e60ff02293fecaacea56498239f59a6175b8a6
Pavel-Kuznetsov-111 <gilrok@rambler.ru>
2024-08-15 22:23:41 +0300

    Убрал лишние пробелы в Задаче №1.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача №1");
-        String firstName = " Ivan ";
+        String firstName = "Ivan";
         String middleName = " Ivanovich";
-        String lastName = " Ivanov ";
+        String lastName = "Ivanov ";
         String fullName = lastName + firstName + middleName;
-        System.out.println("Ф.И.О. сотрудника " + fullName + ". ");
+        System.out.println("Ф.И.О. сотрудника " + fullName + ".");
         System.out.println();
         System.out.println("Задача №2");
         String small = fullName;
